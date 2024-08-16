64ec1662205630d137f63dfc826de2b222d8b688
MohamedEl-naagar <148581349+MohamedEl-naagar@users.noreply.github.com>
2024-08-16 14:23:34 +0200

    Abstract Repository

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,48 @@
+import { FilterQuery, Model, Types, UpdateQuery } from "mongoose";
+import { AbstractDocument } from "./abstract.schema";
+import { Logger, NotFoundException } from "@nestjs/common";
+import { filter } from "rxjs";
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+protected abstract readonly logger: Logger;
+constructor(protected readonly model:Model<TDocument>){}
+
+async create(docuemnt: Omit<TDocument,'_id'>):Promise<TDocument>{
+    const createDocument = new this.model({
+        ...docuemnt,
+        _id: new Types.ObjectId(),
+    });
+    return (await createDocument.save()).toJSON() as unknown as TDocument
+}
+async findOne(filterQuery:FilterQuery<TDocument>):Promise<TDocument>{
+    const docuemnt =    await this.model.findOne(filterQuery).lean<TDocument>(true)
+if(!docuemnt){
+    this.logger.warn('Document not found!',filterQuery) 
+    throw new NotFoundException('doucmnet not found')
+}
+return docuemnt
+}
+
+
+async findOneAndUpdate(
+    filterQuery:FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>
+):Promise<TDocument>{
+    const docuemnt = await this.model.findOneAndUpdate(filterQuery,update,{
+        new:true
+    }).lean<TDocument>(true)
+    if(!docuemnt){
+        this.logger.warn('Document not found!',filterQuery) 
+        throw new NotFoundException('doucmnet not found')
+    }
+    return docuemnt
+}
+
+async find(filterQuery:FilterQuery<TDocument>):Promise<TDocument[]>{
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+}
+async findOneAndDelete(filterQuery:FilterQuery<TDocument>):Promise<TDocument>{
+   return this.model.findOneAndDelete(filterQuery).lean<TDocument>(true)
+
+}
+}
