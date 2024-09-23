fde7ac3967dfc126bb74dc7651a24ed019b3b07e
MohamedEl-naagar <148581349+MohamedEl-naagar@users.noreply.github.com>
2024-09-23 06:59:34 +0200

    separate the dot env

---
+++ apps/auth/src/stratiges/local.startegy.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Starategy) {}
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,16 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { ReservationSchema } from './models/reservation.schema'; // Remove ReservationDocument import if not used elsewhere
 import { ReservationsRepository } from './reservation.repository';
 import { DatabaseModule, LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     DatabaseModule,
-    MongooseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Reservation', schema: ReservationSchema }, // Use the correct model name
+    ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),    
+    
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
