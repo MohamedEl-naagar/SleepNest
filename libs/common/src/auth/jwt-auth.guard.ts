03f00bb6c113d6e6bdcf81a3b2f9b1e009247ce5
MohamedEl-naagar <148581349+MohamedEl-naagar@users.noreply.github.com>
2024-11-28 07:10:14 +0200

    resolve issues for libs common

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -10,7 +10,7 @@ import { ClientProxy } from '@nestjs/microservices';
 import { Reflector } from '@nestjs/core';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 import { AUTH_SERVICE } from '../constants/services';
-import { UserDto } from '../dto/user.dto';
+import { UserDto } from '../dto';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
